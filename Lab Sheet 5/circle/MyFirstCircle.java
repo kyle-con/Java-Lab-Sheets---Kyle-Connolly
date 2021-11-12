a36cd0e0b59a130d0bbc02525a829464265b0777
kyle-con <91945548+kyle-con@users.noreply.github.com>
2021-11-12 21:44:43 +0000

    Add files via upload

---
+++ Lab Sheet 5/circle/MyFirstCircle.java
@@ -0,0 +1,14 @@
+public class MyFirstCircle 
+{ 
+  public static void main(String args[]) 
+  { 
+   Point myPoint = new Point(10, 20); 
+ 
+   Circle myCircle = new Circle(15, 30, 14); 
+ 
+   System.out.println("Point  details  :  "  + 
+myPoint); 
+   System.out.println("Circle  details:  "  + 
+myCircle);
+  }
+}
